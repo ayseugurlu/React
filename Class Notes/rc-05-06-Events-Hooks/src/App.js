bdeaf3448efdaa12fb4df0f58f848846c8f21575
ayseugurlu <ayseugurlu9135@gmail.com>
2024-07-10 11:10:15 +0200

    "Added Form component to App.js and commented out Counter component"

--- Class Notes/rc-05-06-Events-Hooks/src/App.js
+++ Class Notes/rc-05-06-Events-Hooks/src/App.js
@@ -1,6 +1,7 @@
 import Events from "./1-events-hooksIntro/Events";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./3-classComponents/Counter";
+import Form from "./4-form/Form";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -11,7 +12,8 @@ function App() {
       {/* <Events/> */}
 
       {/* <Hooks/> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <Form/>
     </div>
   );
 }
