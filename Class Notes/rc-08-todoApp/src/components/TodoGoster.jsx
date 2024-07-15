e9bdcfb53053a16122526fc4ada0b978f3291caf
ayseugurlu <ayseugurlu9135@gmail.com>
2024-07-15 12:09:54 +0200

    dsg

---
+++ Class Notes/rc-08-todoApp/src/components/TodoGoster.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { MdDeleteForever } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+
+
+
+const TodoGoster = ({ doing, setDoing }) => {
+    const deleteTodo=(id)=>{
+        setDoing(doing.filter((a)=>a.id!==id))
+
+    }
+  return (
+    <div>
+      {doing.map((x) => (
+        <div className={x.isDone? "done" : "gorev"} key={x.id}>
+
+          <h3>
+            {x.text}
+            <div>
+              <FaCheckCircle
+              onClick={()=>setDoing(
+                doing.map((a)=>a.id===x.id? {...a,isDone: !a.isDone} :a)
+              )}
+               style={{ color: "green", fontSize: "20px" }} />
+              <MdDeleteForever
+              onClick={()=>deleteTodo(x.id)}
+               style={{ color: "red", fontSize: "25px" }} />
+            </div>
+          </h3>
+
+          <h6>{x.day}</h6>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoGoster;
---
+++ Class Notes/rc-08-todoApp/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Data from "../helper/data"
+import TodoGoster from '../components/TodoGoster'
+import TodoEkle from '../components/TodoEkle'
+
+const Home = () => {
+
+    const [doing,setDoing]=useState(Data)
+  return (
+    <div>
+        <TodoEkle doing={doing} setDoing={setDoing}/>
+        <TodoGoster doing={doing} setDoing={setDoing}/>
+    </div>
+  )
+}
+
+export default Home
