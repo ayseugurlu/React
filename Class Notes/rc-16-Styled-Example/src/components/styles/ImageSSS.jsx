6e034c05083b30452960cd8d8bc5a1b14903ada5
ayseugurlu <ayseugurlu9135@gmail.com>
2024-08-13 11:44:10 +0200

    styled

---
+++ Class Notes/rc-16-Styled-Example/src/components/styles/ImageSSS.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ImageSSS = styled.img`
+width:350px;
+border:2px solid red;
+
+  
+`;
+
+export const LOGOSSS = styled.img`
+background-color:orange;
+width:300px;
+padding:2rem 0 ;
+`;
+
+export default ImageSSS;
