365b643ae0a7739ec5afc5160159846b993939f2
ayseugurlu <ayseugurlu9135@gmail.com>
2024-08-16 13:13:52 +0200

    "Updated Navbar, added styles, and modified components for About, Details, and Error pages; added routing for new pages and footer component."

--- Class Notes/RC-17-18-RecipeAppProject/src/components/navbar/Navbar.jsx
+++ Class Notes/RC-17-18-RecipeAppProject/src/components/navbar/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles'
+import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
+
+  const [open,setOpen]=useState(false)
+
   return (
-    <div>Navbar</div>
+    <Nav>
+      <Logo to="/home">
+        <i>{"<Clarusway/>"}</i>
+        <span>recipe</span>
+      </Logo>
+
+      <Hamburger onClick={() => setOpen(!open)}>
+          <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu osman={open} onClick={()=> setOpen(!open)}>
+        <MenuLink to="/about">About</MenuLink>
+        {/* <MenuLink >GitHub</MenuLink> */}
+        <a href="https://github.com/"  target='blank'>GitHub</a>
+        <MenuLink to="/">Logout</MenuLink>
+      </Menu>
+    </Nav>
   )
 }
 
--- Class Notes/RC-17-18-RecipeAppProject/src/pages/home/RecipeCard.jsx
+++ Class Notes/RC-17-18-RecipeAppProject/src/pages/home/RecipeCard.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import { RecipeContext } from '../../context/RecipeProvider'
 import { Cards, MainContainer, RecipeButton, RecipeHeader, RecipeImage } from './HomeStyles'
+import { useNavigate } from 'react-router-dom'
 
 
 const RecipeCard = () => {
 
   const {recipes}=useContext(RecipeContext)
+
+  const navigate=useNavigate()
 
   return (
     <MainContainer>
@@ -13,7 +16,7 @@ const RecipeCard = () => {
         <Cards key={index}>
           <RecipeHeader>{recipe.label}</RecipeHeader>
           <RecipeImage src={recipe.image}/>
-          <RecipeButton>Details</RecipeButton>
+          <RecipeButton onClick={()=>navigate("/details", {state:{recipe}})}>Details</RecipeButton>
         </Cards>
       ))}
     </MainContainer>
